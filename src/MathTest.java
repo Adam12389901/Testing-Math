13fce3dd009f17e36e6b4abf01b263bc8f0e7c83
Adam12389901 <128Akonikov@frhsd.com>
2026-09-25 14:55:30 -0400

    Tested the ZeroPower property

--- src/MathTest.java
+++ src/MathTest.java
@@ -110,7 +110,7 @@ public class MathTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testZeroPower(double base) {
-
+        Assertions.assertEquals(Math.pow(base,0),1);
     }
 
 
