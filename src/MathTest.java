652070bdd52cd290f3d90d2b8fd8757f96d78d4a
Adam12389901 <128Akonikov@frhsd.com>
2026-09-25 14:39:32 -0400

    Tested the multiplying property

--- src/MathTest.java
+++ src/MathTest.java
@@ -63,7 +63,7 @@ public class MathTest {
             "-7.4, -8.3, -9.2"
     })
     public void testMultiplying(double base, double a, double b) {
-        xa * xb = xa+b;
+        Assertions.assertEquals(Math.pow(base,a) * Math.pow(base,b),Math.pow(base,a+b));
     }
 
 
